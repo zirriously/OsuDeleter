3ca498b73071c5cca3805190ca2b45ebbb68d0b5
ChristofferP <31447312+zirriously@users.noreply.github.com>
2017-09-28 10:51:38 +0200

    Minor reformatting of code

--- OsuDeleter1/Form1.Designer.cs
+++ OsuDeleter1/Form1.Designer.cs
@@ -182,6 +182,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "OsuDeleter";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- OsuDeleter1/Form1.cs
+++ OsuDeleter1/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace OsuDeleter1
@@ -18,37 +13,32 @@ namespace OsuDeleter1
             InitializeComponent();
         }
 
-        string osuDirectory;
-        DialogResult dialogResult;
+        private string osuDirectory;
+        private DialogResult dialogResult;
+
         public void directoryButton_Click(object sender, EventArgs e)
         {
             folderBrowserDialog1.ShowDialog();
             if (Directory.Exists(folderBrowserDialog1.SelectedPath))
-            {
                 if (folderBrowserDialog1.SelectedPath.Contains("osu"))
                 {
                     if (folderBrowserDialog1.SelectedPath.Contains("\\Songs"))
-                    {
                         osuDirectory = folderBrowserDialog1.SelectedPath;
-                    }
                     else
-                    {
                         osuDirectory = folderBrowserDialog1.SelectedPath + "\\Songs";
-                    }
                 }
                 else
                 {
-                    dialogResult = MessageBox.Show($"It looks like you didn't choose the Osu! directory. The directory you have selected is {folderBrowserDialog1.SelectedPath}. \nAre you sure you want to use this directory?", "Incorrect path", MessageBoxButtons.YesNo);
+                    dialogResult = MessageBox.Show(
+                        $"It looks like you didn't choose the Osu! directory. The directory you have selected is {folderBrowserDialog1.SelectedPath}. \nAre you sure you want to use this directory?",
+                        "Incorrect path", MessageBoxButtons.YesNo);
                     if (dialogResult == DialogResult.Yes)
                         osuDirectory = folderBrowserDialog1.SelectedPath;
                     if (dialogResult == DialogResult.No)
                         MessageBox.Show("Please pick another directory.");
                 }
-            }
             else
-            {
                 MessageBox.Show("The path selected does not exist.");
-            }
             directoryTextBox.Text = osuDirectory;
         }
 
@@ -56,12 +46,12 @@ namespace OsuDeleter1
         {
             //osuDirectory = directoryTextBox.ToString();
         }
-        
 
-        bool jpgFilesChecked = false;
-        bool pngFilesChecked = false;
-        bool wavFilesChecked = false;
-        bool aviFilesChecked = false;
+
+        private bool jpgFilesChecked;
+        private bool pngFilesChecked;
+        private bool wavFilesChecked;
+        private bool aviFilesChecked;
 
         private void jpgFilesTickBox_CheckedChanged(object sender, EventArgs e)
         {
@@ -91,21 +81,13 @@ namespace OsuDeleter1
             //Console.WriteLine($"aviFilesChecked = {aviFilesChecked}");
         }
 
-        private bool CheckboxesActive
-        {
-                get { return (jpgFilesTickBox.Checked == true || pngFilesCheckBox.Checked == true || wavFilesCheckBox.Checked == true || aviFilesCheckBox.Checked == true); }
-        }
+        private bool CheckboxesActive => jpgFilesTickBox.Checked || pngFilesCheckBox.Checked ||
+                                         wavFilesCheckBox.Checked || aviFilesCheckBox.Checked;
 
-        /*
-        private List<String> FileList(String directory, String extension)
-        {
-            return Directory.GetFiles(directory, extension, SearchOption.AllDirectories).ToList();
-        }
-        */
+        private readonly List<string> fileList = new List<string>();
 
-        List<String> fileList = new List<String>();
+        private double count;
 
-        double count;
         private void beginScanButton_Click(object sender, EventArgs e)
         {
             if (osuDirectory == null)
@@ -122,11 +104,13 @@ namespace OsuDeleter1
                 if (aviFilesChecked)
                     fileList.AddRange(Directory.GetFiles(osuDirectory, "*.avi", SearchOption.AllDirectories));
                 if (fileList.Count == 0)
+                {
                     MessageBox.Show("No files have been found. Did you choose the correct directory for Osu?");
+                }
                 else
                 {
                     count = fileList.Count();
-                    amountOfFilesFoundNumberLabel.Text = System.Convert.ToString(count);
+                    amountOfFilesFoundNumberLabel.Text = count.ToString();
                     deleteFilesButton.Enabled = true;
                     amountOfFilesTextLabel.Enabled = true;
                     amountOfFilesFoundNumberLabel.Show();
@@ -136,24 +120,25 @@ namespace OsuDeleter1
 
         private void button1_Click(object sender, EventArgs e)
         {
-         
         }
 
         private void deleteFilesButton_Click(object sender, EventArgs e)
         {
-            dialogResult = dialogResult = MessageBox.Show($"Are you sure you want to delete {fileList.Count()} file(s)?", "", MessageBoxButtons.YesNo);
+            dialogResult = dialogResult =
+                MessageBox.Show($"Are you sure you want to delete {fileList.Count()} file(s)?", "",
+                    MessageBoxButtons.YesNo);
             if (dialogResult == DialogResult.Yes)
             {
                 foreach (var i in fileList)
-                {
-                    //Console.WriteLine("Deleted file " + i);
                     File.Delete(Convert.ToString(i));
-                }
                 MessageBox.Show($"{count} files have been deleted.");
             }
-            if (dialogResult == DialogResult.No)
+            else
                 MessageBox.Show("Deletion cancelled.");
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
     }
 }
