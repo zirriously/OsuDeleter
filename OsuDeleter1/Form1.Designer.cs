bc9b0e680936043ea81ef6945dc211645429712b
ChristofferP <31447312+zirriously@users.noreply.github.com>
2017-09-28 15:29:19 +0200

    Window non-resizeable

--- OsuDeleter1/Form1.Designer.cs
+++ OsuDeleter1/Form1.Designer.cs
@@ -167,6 +167,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(445, 120);
             this.Controls.Add(this.DeleteFilesButton);
             this.Controls.Add(this.amountOfFilesFoundNumberLabel);
@@ -179,8 +180,11 @@
             this.Controls.Add(this.jpgFilesTickBox);
             this.Controls.Add(this.directoryButton);
             this.Controls.Add(this.directoryTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "OsuDeleter";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
