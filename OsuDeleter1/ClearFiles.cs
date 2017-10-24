e54bbca6323988a06ca9eaea74c4c49bf651db67
zirr <chri650t@edu.easj.dk>
2017-10-24 21:19:59 +0200

    Added classes for reorganisation - need to implement threading so it doesn't hang, and actually reorganize the code itself into different classes.

---
+++ OsuDeleter1/ClearFiles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsuDeleter
+{
+    public class ClearFiles
+    {
+        // TO BE ADDED - CLEARS ALL CURRENTLY FOUND FILES AND RESETS BUTTONS
+    }
+}
